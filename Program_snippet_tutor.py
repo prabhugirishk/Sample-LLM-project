002e80eac0796ff12d8088dde8c32d297ae4b79a
Girish Prabhu K <prabhugirish@yahoo.com>
2025-07-26 14:07:31 +0530

    Update Program_snippet_tutor.py

--- Program_snippet_tutor.py
+++ Program_snippet_tutor.py
@@ -79,6 +79,24 @@ def stream_query_ollama(query):
         response = response.replace("```","").replace("markdown", "")
         update_display(Markdown(response), display_id=display_handle.display_id)
 
+#Or use Ollama directly without openai API
+# OLLAMA_API = "http://localhost:11434/api/chat"
+# HEADERS = {"Content-Type": "application/json"}
+# MODEL = "llama3.2"
+# messages = [
+#     {"role": "user", "content": "Describe some of the business applications of Generative AI"}
+# ]
+# payload = {
+#         "model": MODEL,
+#         "messages": messages,
+#         "stream": False
+#     }
+# response = requests.post(OLLAMA_API, json=payload, headers=HEADERS)
+# print(response.json()['message']['content'])
+#response = ollama.chat(model=MODEL, messages=messages)
+#print(response['message']['content'])
+
+
 #Enter the question here Or accept it as user input
 question = """
 Please explain what this code does and why:
